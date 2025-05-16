0c2fe064af3ef6989c5a1bb267e2e7b03e0688d5
Kou-Yeager <wongical552@gmail.com>
2025-05-16 11:20:23 +0000

    mojito: bring up LineageOS based

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_mojito.mk
+    $(LOCAL_DIR)/lineage_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_mojito-user \
-    afterlife_mojito-userdebug \
-    afterlife_mojito-eng
+    lineage_mojito-user \
+    lineage_mojito-userdebug \
+    lineage_mojito-eng
