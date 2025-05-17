fa18dcad78725e413be4efe321122728e8dc042f
Kou-Yeager <wongical552@gmail.com>
2025-05-17 11:09:38 +0700

    mojito: remove non include packages [2]

--- device.mk
+++ device.mk
@@ -190,7 +190,6 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.composer@2.2-resources.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
