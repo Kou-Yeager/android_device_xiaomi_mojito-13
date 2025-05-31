993b1aa7bac8eb58ed84790b24aefeca2b3b80e6
Kou-Yeager <wongical552@gmail.com>
2025-05-31 02:32:05 +0000

    mojito: bring up calyx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_mojito.mk
+    $(LOCAL_DIR)/calyx_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_mojito-user \
-    afterlife_mojito-userdebug \
-    afterlife_mojito-eng
+    calyx_mojito-user \
+    calyx_mojito-userdebug \
+    calyx_mojito-eng
